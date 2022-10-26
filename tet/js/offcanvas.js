482957112b5c11ac51ded298ff453c756d01534e
diego <diego_g_ramirez@hotmail.com>
2022-10-26 18:32:06 -0300

    casi termino el home, tengo que arreglar el carousel

---
+++ tet/js/offcanvas.js
@@ -0,0 +1,2 @@
+(function(){'use strict'
+document.querySelector('#navbarSideCollapse').addEventListener('click',function(){document.querySelector('.offcanvas-collapse').classList.toggle('open')})})()
